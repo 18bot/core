7db48633ba74256ffd4d2dc1b4065dcca7d08b02
Alex Siryi <desertkun@gmail.com>
2018-06-18 01:09:04 +0300

    Initial commit

---
+++ src/utils.h
@@ -0,0 +1,18 @@
+
+#ifndef utils_h
+#define utils_h
+
+#include <stdio.h>
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include <random>
+#include <fstream>
+#include <streambuf>
+#include <functional>
+#include <list>
+
+#include "json/value.h"
+
+#endif /* utils_h */
